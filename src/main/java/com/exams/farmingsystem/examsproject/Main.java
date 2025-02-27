2c7d3ee4d2d3acfd834e69532e3c2b1e35962233
dzrekenathan <dzrekenathan2002@gmail.com>
2025-02-27 14:31:42 +0000

    loader to start up on signup page

--- src/main/java/com/exams/farmingsystem/examsproject/Main.java
+++ src/main/java/com/exams/farmingsystem/examsproject/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("homepage.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("signup.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 500);
         stage.setTitle("Green Harvest");
         stage.setScene(scene);
